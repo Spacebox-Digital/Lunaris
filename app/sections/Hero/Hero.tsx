b3172a2e433b0af1d6ef4e6832f42d3334fd5399
Joshua Michaels <joshuaiz@me.com>
2024-06-20 23:30:21 -0500

    adds vars

--- app/sections/Hero/Hero.tsx
+++ app/sections/Hero/Hero.tsx
@@ -7,30 +7,32 @@ import {HeroSlider} from './HeroSlider';
 import {Schema} from './Hero.schema';
 
 export function Hero({cms}: {cms: HeroCms}) {
-  const {section, slider, slides} = cms;
+    const {section, slider, slides} = cms;
 
-  return (
-    <Container container={cms.container}>
-      <HeroContainer cms={cms}>
-        {slides?.length > 1 && (
-          <HeroSlider
-            aboveTheFold={section?.aboveTheFold}
-            slider={slider}
-            slides={slides}
-          />
-        )}
+    console.log('cms in hero', cms);
 
-        {slides?.length === 1 && (
-          <HeroSlide
-            aboveTheFold={section?.aboveTheFold}
-            isActiveSlide
-            isFirstSlide
-            slide={slides[0]}
-          />
-        )}
-      </HeroContainer>
-    </Container>
-  );
+    return (
+        <Container container={cms.container}>
+            <HeroContainer cms={cms}>
+                {slides?.length > 1 && (
+                    <HeroSlider
+                        aboveTheFold={section?.aboveTheFold}
+                        slider={slider}
+                        slides={slides}
+                    />
+                )}
+
+                {slides?.length === 1 && (
+                    <HeroSlide
+                        aboveTheFold={section?.aboveTheFold}
+                        isActiveSlide
+                        isFirstSlide
+                        slide={slides[0]}
+                    />
+                )}
+            </HeroContainer>
+        </Container>
+    );
 }
 
 Hero.displayName = 'Hero';
